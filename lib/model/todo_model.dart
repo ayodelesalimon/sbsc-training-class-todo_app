928fd4f0971308a2b4e9ebb64f992f219b4f8d9a
Ayodele Salimonu <ayodele951@gmail.com>
2021-05-17 12:52:05 +0100

    first commit

---
+++ lib/model/todo_model.dart
@@ -0,0 +1,13 @@
+class TodoModelClass {
+  final String title;
+  final String description;
+  final String dateTime;
+  final bool doneState;
+
+  TodoModelClass({
+    this.title,
+    this.description,
+    this.dateTime,
+    this.doneState = false,
+  });
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final Color customPrimaryColor = Colors.pink;
+final Color customBackgroundColor = Colors.grey.shade300;
+final Color customScaffoldBackgroundColor = Colors.white;
